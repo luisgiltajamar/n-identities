0511092e787f10c104a5219627dbfd9b6c133bda
lamaslg <lamaslg@gmail.com>
2015-07-01 18:32:40 +0200

    A

---
+++ SeguridadDemo/Controllers/LoginController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using SeguridadDemo.Models;
+
+namespace SeguridadDemo.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View(new Usuario());
+        }
+
+
+        [HttpPost]
+        public ActionResult Index(Usuario model)
+        {
+
+            if ((model.Login == "luis" && model.Password == "luis") ||
+                (model.Login == "natalia" && model.Password == "natalia"))
+            {
+                var prin = new GenericPrincipal(new GenericIdentity(model.Login), new string[] {"Admin"});
+             
+                Session["u"] = model;
+
+                Thread.CurrentPrincipal = prin;
+                FormsAuthentication.SetAuthCookie(model.Login, false);
+
+            }
+            else
+            {
+                ModelState.AddModelError("","Datos de autenticacion incorrectos");
+                return View(model);
+            }
+
+
+
+            return RedirectToAction("Index","Home");
+        }
+    }
+}
---
+++ SeguridadDemo/Models/Usuario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeguridadDemo.Models
+{
+  public  class Usuario
+    {
+      public String Login { get; set; }
+      public String Password { get; set; }
+
+    }
+}
